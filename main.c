78401a696d8153f268f484a27020f14a7b53ff2a
Prasoon Rai <arenodev.python@gmail.com>
2025-12-01 15:36:15 +0530

    Implemented Shaders for space time fabric.

--- main.c
+++ main.c
@@ -1,12 +1,34 @@
 #include <stdio.h>
 #include <raylib.h>
+#include <raymath.h>
 
 const int HEIGHT = 800;
 const int WIDTH = 1200;
 
+Shader gridShader;
+
 int main(void) {
     InitWindow(WIDTH, HEIGHT, "CLow");
     SetTargetFPS(60);
+
+    gridShader = LoadShader("grid_vs.glsl", "grid_fs.glsl");
+
+    const float GRID_SPACING = 1.0f;
+    const float GRID_WIDTH = 0.02f;
+    const float QUAD_SIZE = 1000.0f;
+
+    int spacingLoc = GetShaderLocation(gridShader, "gridSpacing");
+    int widthLoc = GetShaderLocation(gridShader, "gridWidth");
+    int lineColorLoc = GetShaderLocation(gridShader, "lineColor");
+
+    Vector4 lineColor = { 0.5f, 0.5f, 0.5f, 1.0f }; // Dark Gray
+    SetShaderValue(gridShader, spacingLoc, &GRID_SPACING, SHADER_UNIFORM_FLOAT);
+    SetShaderValue(gridShader, widthLoc, &GRID_WIDTH, SHADER_UNIFORM_FLOAT);
+    SetShaderValue(gridShader, lineColorLoc, &lineColor, SHADER_UNIFORM_VEC4);
+
+    Model gridModel = LoadModelFromMesh(GenMeshPlane(QUAD_SIZE, QUAD_SIZE, 1, 1));
+    gridModel.materials[0].shader = gridShader;
+
 
     Camera3D camera = { 0 };
     camera.position = (Vector3){ 4.0f, 4.0f, 4.0f };
@@ -17,20 +39,19 @@ int main(void) {
 
     while (!WindowShouldClose()) {
         UpdateCamera(&camera, CAMERA_FREE);
-
         BeginDrawing();
         ClearBackground(BLACK);
-
         BeginMode3D(camera);
 
-        DrawSphere((Vector3){ 0.0f, 1.0f, 0.0f }, 3.0f, BLACK);
+        DrawModel(gridModel, (Vector3){ 0.0f, 0.0f, 0.0f }, 1.0f, WHITE);
 
-        DrawGrid(50, 1.0f);
+        DrawSphere((Vector3){0.0f, 1.0f, 0.0f}, 1.0f, BLACK);
 
         EndMode3D();
         EndDrawing();
     }
-
+    UnloadModel(gridModel);
+    UnloadShader(gridShader);
     CloseWindow();
     return 0;
 }
